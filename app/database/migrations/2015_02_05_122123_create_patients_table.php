e9ed96adde9227d92e3043c7912227ed5ec897cb
Gayan Virajith <gayanvirajith@gmail.com>
2015-02-06 11:56:06 +0530

    Modified field order on patient migration

--- app/database/migrations/2015_02_05_122123_create_patients_table.php
+++ app/database/migrations/2015_02_05_122123_create_patients_table.php
@@ -16,30 +16,30 @@ class CreatePatientsTable extends Migration {
 		{
 			$table->increments('id');
 			$table->string('stroke_id')->unique();
-			$table->tinyInteger('bht_number');
-			$table->integer('hospital_id');
 			$table->string('name');
-			$table->tinyInteger('age');
-			$table->date('dob');
 			$table->string('nic', 10);
 			$table->string('sex', 1);
-			$table->tinyInteger('marital_status');
-			$table->boolean('pregnant'); // if femail, 
-			$table->tinyInteger('pregnant_status'); // if femail, 
-			$table->tinyInteger('ethnicity'); 
-			$table->tinyInteger('dexterity'); 
 			$table->string('address_1'); 
 			$table->string('address_2');
-			$table->tinyInteger('province');  
 			$table->string('contact_no_1', 20);
 			$table->string('contact_no_2', 20);
 			$table->string('guardian_name');
 			$table->string('guardian_contact_no_1', 20);
 			$table->string('guardian_contact_no_2', 20);
+			$table->date('dob');
+			$table->tinyInteger('age');
+			$table->tinyInteger('bht_number');
+			$table->boolean('pregnant'); // if femail, 
+			$table->tinyInteger('marital_status');
+			$table->tinyInteger('pregnant_status'); // if femail, 
+			$table->tinyInteger('ethnicity'); 
+			$table->tinyInteger('dexterity'); 
+			$table->tinyInteger('province');  
 			$table->tinyInteger('education');  
 			$table->tinyInteger('employement');  
 			$table->tinyInteger('level_of_independence');  
 			$table->tinyInteger('living_arrangement');  
+			$table->integer('hospital_id');
 			$table->index('stroke_id');  
 			$table->index('nic');  
 			$table->index('hospital_id');  
