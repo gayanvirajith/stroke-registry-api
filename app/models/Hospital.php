c6d1bbbfcd7e07541c764d06b5445016e9c7aeab
Gayan Virajith <gayanvirajith@gmail.com>
2015-02-06 09:52:13 +0530

    Add Hospital model

--- app/models/Hospital.php
+++ app/models/Hospital.php
@@ -4,8 +4,19 @@ class Hospital extends \Eloquent {
 	
   protected $guarded = ['id'];
 
+  /*
+   * ORM: Hospital has many patients
+   */
   public function patients()
   {
       return $this->hasMany('Patient');
+  }
+
+  /*
+   * ORM: Hospital has many users
+   */
+  public function users()
+  {
+      return $this->hasMany('Users');
   }
 }
