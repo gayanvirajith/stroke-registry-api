58c460f84dbbc30bd327e43f3129bc8d5a580e6f
Gayan Virajith <gayanvirajith@gmail.com>
2015-02-09 16:21:29 +0530

    Add drug history validations

---
+++ app/models/core/builders/DrugHistoryBuilder.php
@@ -0,0 +1,63 @@
+<?php
+
+class DrugHistoryBuilder {
+
+  /**
+   * @var drugHistory
+   */ 
+  protected $drugHistory;
+
+
+  /**
+   * @var 
+   */
+  protected $config = array();
+
+
+  /**
+   * Parameterized constructor with:
+   *
+   * @param array $config
+   * @param DrugHistory $drugHistory
+   */
+  public function __construct(array $config, DrugHistory $drugHistory = NULL) {
+
+    $this->drugHistory = $drugHistory; 
+    $this->setConfig($config);
+  }
+
+  /**
+   * Process some default configurations
+   * 
+   * @param array $config
+   */
+  protected function setConfig(array $config) {
+    // todo set some default values
+    $defaults = array();
+
+    $config =  array_merge($defaults, $config);
+
+    $this->config = $config;
+  }
+
+  /**
+   * Build the drugHistory using the supplied configuration parameters
+   *
+   * @return null
+   */
+  public function build()
+  {
+      foreach ($this->config as $option => $value) {
+        $this->drugHistory->$option = $value;
+      }
+  }
+
+
+  /**
+   * @return drugHistory
+   */
+  public function getDrugHistory()
+  {
+      return $this->drugHistory;
+  }
+}
