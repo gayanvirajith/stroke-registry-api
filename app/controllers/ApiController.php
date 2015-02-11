be6d12d9cbc467d56f133ffadd5cd671a5c3de95
Gayan Virajith <gayanvirajith@gmail.com>
2015-02-11 18:52:28 +0530

    Refactoring Respond messages

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -1,6 +1,71 @@
 <?php
 
+/**
+ * Class ApiController
+ */
+class ApiController extends BaseController {
 
-class ApiController {
+    /**
+     * @var
+     */
+    protected $statusCode = 200;
 
+    /**
+     * @return mixed
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * @param mixed $statusCode
+     * @return $this
+     */
+    public function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+        // Returns the object for method chaining.
+        return $this;
+    }
+
+    /**
+     * @param string $message
+     * @return mixed
+     */
+    public function respondNotFound($message = 'Not Found!') {
+        return $this->setStatusCode(404)->respondWithError($message);
+    }
+
+    /**
+     * @param $message
+     * @return mixed
+     */
+    public function respondUnauthorized($message) {
+        return $this->setStatusCode(401)->respondWithError($message);
+    }
+
+
+    /**
+     *
+     * @param $data
+     * @param array $headers
+     * @return mixed
+     */
+    public function respond($data, $headers = []) {
+        return Response::json($data, $this->getStatusCode(), $headers);
+    }
+
+    /**
+     * @param $message
+     * @return mixed
+     */
+    public function respondWithError($message) {
+        return Response::json([
+            'error' => [
+                'message' => $message,
+                'status_code' => $this->getStatusCode()
+            ]
+        ]);
+    }
 }
